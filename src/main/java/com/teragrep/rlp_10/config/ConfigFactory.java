a7e819a946d41f3ee495be3b86ee6e0f3e705442
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-09-24 11:46:39 +0300

    Spotless & Checkstyle

--- src/main/java/com/teragrep/rlp_10/config/ConfigFactory.java
+++ src/main/java/com/teragrep/rlp_10/config/ConfigFactory.java
@@ -60,7 +60,9 @@ public final class ConfigFactory {
     private final Map<String, String> configValues;
 
     /**
-     * Takes key-value pairs as a Map of Strings and creates various Config objects based on values received, applying defaults for any omitted values.
+     * Takes key-value pairs as a Map of Strings and creates various Config objects based on values received, applying
+     * defaults for any omitted values.
+     * 
      * @param configValues Map<String,String> containing configuration key-value pairs
      */
     public ConfigFactory(final Map<String, String> configValues) {
